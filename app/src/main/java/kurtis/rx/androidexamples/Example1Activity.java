b1283a653f0d732aa47e3543855caf79fd58994e
Eric Liu <eric@loveagency.com.au>
2016-11-07 15:02:18 +1100

    play defer

--- app/src/main/java/kurtis/rx/androidexamples/Example1Activity.java
+++ app/src/main/java/kurtis/rx/androidexamples/Example1Activity.java
@@ -1,26 +1,51 @@
 package kurtis.rx.androidexamples;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import rx.Observable;
 import rx.Observer;
+import rx.functions.Func0;
 
 public class Example1Activity extends AppCompatActivity {
 
     RecyclerView mColorListView;
     SimpleStringAdapter mSimpleStringAdapter;
+    private long num = 0L;
+    private Observable<Long> deferObservable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         configureLayout();
         createObservable();
+
+        Handler handler = new Handler();
+        for (int i = 0; i < 5; i++) {
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    num += 1;
+                }
+            }, 1000 * i );
+        }
+
+        deferObservable = Observable.defer(new Func0<Observable<Long>>(){
+            @Override
+            public Observable<Long> call() {
+                return Observable.just(num);
+            }
+        });
     }
 
     private void createObservable() {
@@ -62,5 +87,43 @@ public class Example1Activity extends AppCompatActivity {
         colors.add("chartreuse");
         colors.add("Van Dyke Brown");
         return colors;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_defer) {
+
+            deferObserverable();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deferObserverable() {
+
+        deferObservable.subscribe(new Observer<Long>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                Toast.makeText(Example1Activity.this, "The value of num: " + num, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
